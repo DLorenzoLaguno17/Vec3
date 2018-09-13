97721f755ea43619af4fc7cd3ba4ee0cdbefb178
DLorenzoLaguno17 <36265574+DLorenzoLaguno17@users.noreply.github.com>
2018-09-13 19:12:09 +0200

    All methods implemented

--- Vec3/Vec3/Vec3.h
+++ Vec3/Vec3/Vec3.h
@@ -6,10 +6,40 @@ template<class TYPE>
 class Vec3 {
 public:
 	//Methods
-	Vec3 normalize() = 0;
-	Vec3 zero() = 0;
-	bool is_zero() = 0;
-	float distance_yo(Vec3* v) = 0;
+	Vec3 normalize() {
+		Vec3 newVec3;
+		
+		newVec3.x = x / (sqrt((x) ^ 2 + (y) ^ 2 + (z) ^ 2));
+		newVec3.y = y / (sqrt((x) ^ 2 + (y) ^ 2 + (z) ^ 2);
+		newVec3.y = z / (sqrt((x) ^ 2 + (y) ^ 2 + (z) ^ 2));
+
+		return newVec3;
+	}
+
+	void zero() {
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+
+	bool is_zero() {
+		if (x == 0 && y == 0 && z == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	float distance_to(Vec3* v) {		
+		float dx = v->x - x;
+		float dy = v->y - y;
+		float dz = v->z - z;
+
+		dist = sqrt((dx) ^ 2 + (dy) ^ 2 + (dz) ^ 2);
+
+		return abs(dist);
+	}
 
 	//Operators
 
