363b3142aa35a055a23963324e561bc28f504046
DLorenzoLaguno17 <36265574+DLorenzoLaguno17@users.noreply.github.com>
2018-09-13 22:26:51 +0200

    3 operators implemented

--- Vec3/Vec3/Vec3.h
+++ Vec3/Vec3/Vec3.h
@@ -5,12 +5,19 @@ template<class TYPE>
 
 class Vec3 {
 public:
+	//Constructors
+	/*Vec3() {}
+
+	Vec3() : x(0), y(0), z(0) {}
+
+	Vec3(TYPE x_, TYPE y_, TYPE z_) : x(z_), y(y_), z(z_){}*/
+
 	//Methods
 	Vec3 normalize() {
 		Vec3 newVec3;
 		
 		newVec3.x = x / (sqrt((x) ^ 2 + (y) ^ 2 + (z) ^ 2));
-		newVec3.y = y / (sqrt((x) ^ 2 + (y) ^ 2 + (z) ^ 2);
+		newVec3.y = y / (sqrt((x) ^ 2 + (y) ^ 2 + (z) ^ 2));
 		newVec3.y = z / (sqrt((x) ^ 2 + (y) ^ 2 + (z) ^ 2));
 
 		return newVec3;
@@ -22,16 +29,11 @@ public:
 		z = 0;
 	}
 
-	bool is_zero() {
-		if (x == 0 && y == 0 && z == 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
+	bool is_zero() const {
+		return (x == 0 && y == 0 && z == 0);
 	}
 
-	float distance_to(Vec3* v) {		
+	float distance_to(Vec3* v) const {		
 		float dx = v->x - x;
 		float dy = v->y - y;
 		float dz = v->z - z;
@@ -42,6 +44,56 @@ public:
 	}
 
 	//Operators
+	Vec3 operator+(const Vec3 &v) const {
+		Vec3 newVec3;
+
+		newVec3.x = x + v.x;
+		newVec3.y = y + v.y;
+		newVec3.z = z + v.z;
+
+		return newVec3;
+	}
+
+	Vec3 operator-(const Vec3 &v) const {
+		Vec3 newVec3;
+
+		newVec3.x = x - v.x;
+		newVec3.y = y - v.y;
+		newVec3.z = z - v.z;
+
+		return newVec3;
+	}
+
+	Vec3 operator+=(const Vec3 &v) const {
+		Vec3 newVec3;
+
+		newVec3.x = x + v.x;
+		newVec3.y = y + v.y;
+		newVec3.z = z + v.z;
+
+		return newVec3;
+	}
+
+	Vec3 operator-=(const Vec3 &v) const {
+		Vec3 newVec3;
+
+		newVec3.x = x - v.x;
+		newVec3.y = y - v.y;
+		newVec3.z = z - v.z;
+
+		return newVec3;
+	}
+
+	Vec3 operator=(const Vec3 &v) const {
+		Vec3 newVec3 = { v.x, v.y, v.z };
+
+		return newVec3;
+	}
+
+	bool operator==(const Vec3 &v) const {
+		return (x == v.x && y == v.y && z == v.z);
+	}
+	
 
 private:
 	TYPE x, y, z;
--- Vec3/Vec3/main.cpp
+++ Vec3/Vec3/main.cpp
@@ -1,4 +1,12 @@
-int main() {
+#include <iostream>
+#include "Vec3.h"
 
+int main() {
+	Vec3<int> intVector = {};
+	Vec3<float> floatVector = {};
+
+	std::cout << "Nip de roubot" << std::endl;
+
+	system("pause");
 	return 0;
 }
