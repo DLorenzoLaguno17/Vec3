f807374b6daf4d5c915aafadcf8a8f2ee835199e
DLorenzoLaguno17 <36265574+DLorenzoLaguno17@users.noreply.github.com>
2018-09-13 17:39:07 +0200

    Created half of the header (methods)

--- Vec3/Vec3/Vec3.h
+++ Vec3/Vec3/Vec3.h
@@ -1,11 +1,20 @@
 #ifndef  _VEC3_H_
 #define _VEC3_H_
 
+template<class TYPE>
+
 class Vec3 {
 public:
+	//Methods
+	Vec3 normalize() = 0;
+	Vec3 zero() = 0;
+	bool is_zero() = 0;
+	float distance_yo(Vec3* v) = 0;
+
+	//Operators
 
 private:
-
+	TYPE x, y, z;
 };
 
 #endif // _VEC3_H_
